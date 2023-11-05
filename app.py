f8dfb05e22d5c16ffaa58e7572e268d2775e64ca
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2023-11-05 12:22:15 +0000

    Add log out functionality

--- app.py
+++ app.py
@@ -83,7 +83,19 @@ def profile(username):
     # obtain the session user's username from the db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been successfully logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
